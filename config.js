0a03a9f131d5fa84ffd446033c977d1c1434ade7
eaxandm-gif <ea.xandm@gmail.com>
2026-07-03 17:44:04 +0700

    Fix syntax error in liffId string

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = Object.freeze({
-  liffId: '2010559182-kPsdNnbd,
+  liffId: '2010559182-kPsdNnbd',
   gasWebAppUrl: 'https://script.google.com/macros/s/AKfycbxdbCqBI3HRDfeyil5WJRSvrKbKHf8KlMejlGLy3bw1qVEFSxtHspxAAGOUKVImc2zumQ/exec'
 });
